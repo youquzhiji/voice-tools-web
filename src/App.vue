057f0cdb5d1210c7b54b631f825aef2b8dc0080b
wuliaozhiji <youquzhiji@gmail.com>
2022-02-06 12:04:20 -0500

    [S] Style router links

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
     <!-- use the router-link component for navigation. -->
     <!-- specify the link by passing the `to` prop. -->
     <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/">Go to Home</router-link>
-    <router-link to="/about">Go to About</router-link>
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
   </p>
 
   <!-- route outlet -->
