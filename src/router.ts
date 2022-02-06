daf3725986fb8f57609d64f5a64b9247d4a31fc2
wuliaozhiji <youquzhiji@gmail.com>
2022-02-06 16:56:18 -0500

    [+] Create about view

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
 import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
-    {path: '/', name: 'Home', component: Home}
+    {path: '/', name: 'Home', component: Home},
+    {path: '/about', name: 'About', component: About}
 ]
 
 const router = createRouter({
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  This is an about page.
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
