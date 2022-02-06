61a6b248409d2a906893e9d1b93197ddb842c02e
wuliaozhiji <youquzhiji@gmail.com>
2022-02-06 17:20:20 -0500

    [+] Create drop boxx

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
   Hello World!
+
+  <div class="drop-box unselectable">
+    <span class="drop-label">Drop Here</span>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +11,19 @@
 </script>
 
 <style lang="sass">
+.drop-box
+  width: 100%
+  height: 100px
+  margin-top: 20px
+  border: 5px dashed pink
+  border-radius: 25px
+
+  display: flex
+  align-items: center
+  justify-content: center
+
+  .drop-label
+    color: gray
+    font-size: 1.5em
 
 </style>
