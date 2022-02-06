0a33e17a2f9228956af43631c83cb5041e95b4ca
Hykilpikonna <me@hydev.org>
2022-02-06 18:14:23 -0500

    [+] Read file on drop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,37 @@
 <template>
   Hello World!
 
-  <div class="drop-box unselectable">
+  <div class="drop-box unselectable" @dragover="(e) => e.preventDefault()" @drop="onDrop">
     <span class="drop-label">Drop Here</span>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ElMessage} from "element-plus";
 
+function onDrop(e: DragEvent)
+{
+  // Prevent default behavior: Opening the file in a browser tab.
+  e.preventDefault()
+
+  // Read file data
+  if (e.dataTransfer && e.dataTransfer.items)
+  {
+    // Loop through all files
+    for (let i = 0; i < e.dataTransfer.items.length; i++)
+    {
+      // Check file type
+      const item = e.dataTransfer.items[i]
+      if (item.kind == 'file')
+      {
+        // Read file
+        const file = item.getAsFile()!
+        file.text().then(it => console.log(it))
+      }
+      else ElMessage.error(`Error: The item dropped must be a file, not a ${item.kind}`)
+    }
+  }
+}
 </script>
 
 <style lang="sass">
