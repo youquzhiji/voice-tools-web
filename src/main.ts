e7801dfb346896f1fe344927258851a0f568eb83
Hykilpikonna <me@hydev.org>
2022-02-25 17:47:06 -0500

    [+] Font awesome

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import router from "./router";
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
+import '@fortawesome/fontawesome-free/css/solid.min.css'
 
 createApp(App).use(router).use(ElementPlus).mount('#app')
