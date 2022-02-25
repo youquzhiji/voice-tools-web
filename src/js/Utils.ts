7240743e9b8fc5ba467f58dbe2a9bbb27c19d0a3
wuliaozhiji <youquzhiji@gmail.com>
2022-02-24 22:08:08 -0500

    [+] extremes(a): Calculate min and max

--- src/js/Utils.ts
+++ src/js/Utils.ts
@@ -1,10 +1,34 @@
+type NumberArray = Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array |
+    Float32Array | Float64Array | number[] | Array<number>
 
-export function sum(arr: Array<number>)
+export function sum(arr: NumberArray): number
 {
-    return arr.reduce((a, b) => a + b, 0)
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) sum += arr[i]
+    return sum
 }
 
-export function mean(arr: Array<number>)
+export function mean(arr: NumberArray): number
 {
     return sum(arr) / arr.length
+}
+
+/**
+ * Calculates min and max of an array
+ *
+ * @param a Array
+ * @return [min, max]
+ */
+function extremes(a: NumberArray): [number, number]
+{
+    let min = a[0]
+    let max = a[0]
+
+    for (let n = 0; n < a.length; n++)
+    {
+        if (a[n] < min) min = a[n]
+        if (a[n] > max) max = a[n]
+    }
+
+    return [min, max]
 }
