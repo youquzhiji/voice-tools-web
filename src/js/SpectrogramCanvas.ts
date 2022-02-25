9d467cf5c06670792a9d20b1373d1392d3bbb86b
wuliaozhiji <youquzhiji@gmail.com>
2022-02-24 22:42:09 -0500

    [+] Draw one frame

--- src/js/SpectrogramCanvas.ts
+++ src/js/SpectrogramCanvas.ts
@@ -18,17 +18,33 @@ export default class SpectrogramCanvas extends CanvasController
      */
     drawAudio(audio: AudioBuffer, audioCtx: AudioContext)
     {
-        const source = audioCtx.createBufferSource()
-        const analyzer = meyda.createMeydaAnalyzer({
-            audioContext: audioCtx,
-            source,
-            bufferSize: 2048,
-            featureExtractors: ['amplitudeSpectrum'],
-            callback: (features: any) => {
-                console.log(features.amplitudeSpectrum)
-                analyzer.stop()
-            }
-        })
-        analyzer.start()
+        meyda.sampleRate = 16000
+        meyda.bufferSize = 2048
+        const d = meyda.extract(['amplitudeSpectrum'], audio.getChannelData(0).subarray(0, 2048))!.amplitudeSpectrum!
+
+        console.log(d)
+        const [min, max] = extremes(d)
+        const range = max - min
+
+        // this.el.height = this.h = d.length
+        const pxLen = d.length / this.h
+
+        console.log(pxLen)
+
+        console.log(min, max)
+        const gradient = chroma.scale(['#000',
+            '#4F1879', '#B43A78', '#F98766', '#FCFAC0']);
+
+        for (let y = 0; y < this.h; y++)
+        {
+            const area = d.subarray(y * pxLen, (y + 1) * pxLen)
+
+            this.ctx.beginPath()
+            this.ctx.strokeStyle = gradient((mean(area) - min) / range).toString()
+            this.ctx.moveTo(0, this.h - y)
+            this.ctx.lineTo(this.w, this.h - y)
+            this.ctx.stroke()
+            this.ctx.closePath()
+        }
     }
 }
