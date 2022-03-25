c25c9fe89b7d3bdf862919f48c22b989aa58f152
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-03-25 05:23:25 -0400

    [S] Style results navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home" ref="el" class="fbox-v">
-    <div class="usage">
+    <div class="usage" v-if="!audio">
       Welcome to the voice training tool [TODO: 想一个名字]
     </div>
 
@@ -11,14 +11,21 @@
     <div class="results" :style="{visibility: audio ? 'unset' : 'hidden'}">
       <Waveform :audio="audio" v-if="audio" />
 
-      <div class="result-tabs">
-        <div class="tab-button" @click="() => activeTab = 1">Spectrogram</div>
-        <div class="tab-button" @click="() => activeTab = 2">Classification</div>
-        <div class="tab-button" @click="() => activeTab = 3">Pitch & Formant</div>
+      <div class="result-nav unselectable">
+        <div class="tab-button" @click="() => activeTab = 1" :class="{sel: activeTab === 1}">
+          Spectrogram
+        </div>
+        <div class="tab-button" @click="() => activeTab = 2" :class="{sel: activeTab === 2}">
+          Classification
+        </div>
+        <div class="tab-button" @click="() => activeTab = 3" :class="{sel: activeTab === 3}">
+          Pitch & Formant
+        </div>
       </div>
 
+      <!-- Spectrogram View -->
       <div class="result-tab" v-if="activeTab === 1">
-        <Spectrogram :audio="audio" v-if="audio" />
+<!--        <Spectrogram :audio="audio" v-if="audio" />-->
       </div>
     </div>
   </div>
@@ -90,6 +97,9 @@ export default class Home extends Vue
 
   .drop-box
     width: 100%
+    max-width: 600px
+    margin-left: auto
+    margin-right: auto
     height: 100px
     border: 5px dashed pink
     border-radius: 25px
@@ -108,4 +118,39 @@ export default class Home extends Vue
     flex-direction: column
     flex: 1
     min-height: 0
+
+  $border-color: rgba(95, 55, 38, 0.78)
+
+  .result-tab
+    border: 3px solid $border-color
+    border-top: none
+    margin-top: -1px
+
+  .result-nav
+    display: flex
+    flex-direction: row
+    margin-top: 10px
+    justify-content: center
+    min-height: 30px
+
+    .tab-button
+      padding: 10px
+      display: flex
+      flex-direction: column
+      justify-content: center
+      border-top: 3px solid transparent
+      border-bottom: 3px solid $border-color
+
+    .tab-button.sel
+      border-left: 3px solid $border-color
+      border-right: 3px solid $border-color
+      border-top: 3px solid $border-color
+      border-bottom: 3px solid transparent
+      border-radius: 10px 10px 0 0
+      background: #fff4eb
+
+  .result-nav::before, .result-nav::after
+    content: ''
+    flex-grow: 1
+    border-bottom: 3px solid $border-color
 </style>
