8d1f40e5042605bfd7bb31983170ebb5293f1159
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-03-25 21:53:17 -0400

    [+] Register chartjs

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,10 @@ import 'element-plus/dist/index.css'
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css'
 import '@fortawesome/fontawesome-free/css/solid.min.css'
 
+import {Chart, registerables} from 'chart.js';
+import annotationPlugin from 'chartjs-plugin-annotation';
+
+Chart.register(...registerables)
+Chart.register(annotationPlugin)
+
 createApp(App).use(router).use(ElementPlus).mount('#app')
