2be7a219ce4343e635b0822239a8ffbfe35a4d8f
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-04-04 19:51:49 -0400

    [S] Style about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  This is an about page.
+  <div id="about" class="card shadow">
+    This is an about page.
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -11,5 +13,10 @@ signal.rfft().print()
 </script>
 
 <style lang="sass" scoped>
-
+#about
+  max-width: 600px
+  width: 100%
+  height: 100%
+  margin: 20px auto 40px
+  padding: 20px
 </style>
